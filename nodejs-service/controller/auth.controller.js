1208703e0d844aa7faee75c508192a7cb6ce12ed
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2026-02-16 15:07:47 +0530

    feat: Initialize Node.js backend service with core dependencies, user authentication, and image upload functionality.

--- nodejs-service/controller/auth.controller.js
+++ nodejs-service/controller/auth.controller.js
@@ -1,72 +1,77 @@
-import prisma from "../config/prisma.js"
-import { asyncHandler } from "../utils/asyncHandler.js"
-import bcrypt from "bcrypt"
-import { generateToken } from "../config/generateToken.js"
-import { uploadImage } from "../utils/upload.image.js"
+import prisma from "../config/prisma.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import bcrypt from "bcrypt";
+import { generateToken } from "../config/generateToken.js";
+import imagekit from "../config/imagekit.js";
 
 export const register = asyncHandler(async (req, res) => {
-    const { name, email, password, imageUrl } = req.body
+  const { name, email, password } = req.body;
 
-    const existingUser = await prisma.user.findUnique({ where: { email } })
+  const existingUser = await prisma.user.findUnique({ where: { email } });
 
-    if (existingUser) {
-        return res.status(400).json({ message: "Email already in use" })
-    }
+  if (existingUser) {
+    return res.status(400).json({ message: "Email already in use" });
+  }
 
-    if (!req.file) {
-        return res.status(400).json({ message: "Profile image is required", success: false})
-    }
+  if (!req.file) {
+    return res
+      .status(400)
+      .json({ message: "Profile image is required", success: false });
+  }
 
-    const image = await uploadImage(imageUrl, "image")
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-    const hashedPassword = await bcrypt.hash(password, 10)
+  const uploadResponse = await imagekit.upload({
+    file: req.file.buffer,
+    fileName: req.file.originalname,
+    folder: "/circlo/users",
+  });
 
-    const user = await prisma.user.create({
-        data: {
-            name,
-            email,
-            password: hashedPassword,
-            imageUrl: image.secure_url,
-            imageUrlID: image.public_id,
-        }
-    })
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+      imageUrl: uploadResponse.url,
+      imageUrlID: uploadResponse.fileId,
+    },
+  });
 
-    const token = generateToken(user.id)
+  const token = generateToken(user.id);
 
-    res.status(201).json({ user, token })
-})
+  res.status(201).json({ user, token });
+});
 
 export const login = asyncHandler(async (req, res) => {
-    const { email, password } = req.body
+  const { email, password } = req.body;
 
-    const user = await prisma.user.findUnique({ where: { email } })
+  const user = await prisma.user.findUnique({ where: { email } });
 
-    if (!user) {
-        return res.status(404).json({ message: "User not found" })
-    }
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
 
-    const isPasswordValid = await bcrypt.compare(password, user.password)
+  const isPasswordValid = await bcrypt.compare(password, user.password);
 
-    if (!isPasswordValid) {
-        return res.status(401).json({ message: "Invalid password" })
-    }
+  if (!isPasswordValid) {
+    return res.status(401).json({ message: "Invalid password" });
+  }
 
-    const token = generateToken(user.id)
+  const token = generateToken(user.id);
 
-    res.status(200).json({ user, token })
-})
+  res.status(200).json({ user, token });
+});
 
 export const logout = asyncHandler(async (req, res) => {
-
-    res.status(200).json({ message: "Logout successful" })
-})
+  res.status(200).json({ message: "Logout successful" });
+});
 
 export const getProfile = asyncHandler(async (req, res) => {
-    const user = await prisma.user.findUnique({ where: { id: req.user.id } })
+  const user = await prisma.user.findUnique({ where: { id: req.user.id } });
 
-    if (!user) {
-        return res.status(404).json({ message: "User not found" })
-    }
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
 
-    res.status(200).json({ user })
-})
+  res.status(200).json({ user });
+});
--- nodejs-service/server.js
+++ nodejs-service/server.js
@@ -1,17 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import morgan from "morgan";
 
 dotenv.config();
 
 import authRouter from "./router/auth.router.js"
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 4001;
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"))
 
 app.use("/api/auth", authRouter)
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port http://localhost:${PORT}`);
 });
