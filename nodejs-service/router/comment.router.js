8d69512ba2dd8459c0170b64f1b7cf71dacb08cc
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2026-03-05 13:18:23 +0530

    feat: Add replies and comment like functionality to comment router

--- nodejs-service/router/comment.router.js
+++ nodejs-service/router/comment.router.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import protect from '../middleware/authMiddleware.js';
-import { addComment, deleteComment, getPostComments, updateComment } from '../controller/comment.controller.js';
+import { addComment, deleteComment, getPostComments, getReplies, toggleCommentLike, updateComment } from '../controller/comment.controller.js';
 
 const router = express.Router();
 
@@ -8,5 +8,9 @@ router.route("/:id").post(protect, addComment);
 router.route("/:id").get(protect, getPostComments);
 router.route("/:id").delete(protect, deleteComment);
 router.route("/:id").put(protect, updateComment);
+
+router.route("/reply/:id").get(protect, getReplies);
+
+router.route("/comment-like/:id").post(protect, toggleCommentLike);
 
 export default router;
